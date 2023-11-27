34a592a5eabd7bf62cf7c369c87b3bc9adb2b4b7
DArzoEsp <davidarzola80@gmail.com>
2023-11-26 22:35:58 -0800

    Finished animating overlay for hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 const hamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
-menuClicked = () => (header.classList.contains('open') ? header.classList.remove('open') : header.classList.add('open')); 
+// close and open hamburger menu                                      // close
+menuClicked = () => {
+    if (header.classList.contains('open')) {
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } else {
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
 
+}; 
+
+// when menu is clicked do the function above
 hamburger.addEventListener('click', menuClicked);
+
