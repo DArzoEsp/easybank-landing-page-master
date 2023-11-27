275cb874ea2c9ff563a3727678837c65fbb24c6f
DArzoEsp <davidarzola80@gmail.com>
2023-11-27 02:13:50 -0800

    I finished the menu animation pop up

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
-const hamburger = document.querySelector('#btnHamburger');
-const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay');
+const hamburger = document.querySelector('#btnHamburger');  // selecting hamburger id
+const body = document.querySelector('body');
+const header = document.querySelector('.header');   // selecting header class
+const overlay = document.querySelector('.overlay'); // selecting overlay class
+const fadeElems = document.querySelectorAll('.has-fade');   // selecting all has fade class
 
-// close and open hamburger menu                                      // close
+// close and open hamburger menu                                 
 menuClicked = () => {
     if (header.classList.contains('open')) {
-        header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        body.classList.remove('noscroll');
+        header.classList.remove('open');                            // removes previous opening
+        fadeElems.forEach((e) => e.classList.add('fade-out'));      // fades out with the overlay and menu option
+        fadeElems.forEach((e) => e.classList.remove('fade-in'));    // removes it for multiple times of clicks
+        overlay.classList.remove('fade-in');                        // removes fade in for multiple clicks
+        overlay.classList.add('fade-out');                          // appealing animation of fading out
     } else {
-        header.classList.add('open');
-        overlay.classList.remove('fade-out');
+        body.classList.add('noscroll');
+        header.classList.add('open');   
+        fadeElems.forEach((e) => e.classList.add('fade-in'));       // fades in the overlay with the overlay and menu options
+        fadeElems.forEach((e) => e.classList.remove('fade-out'));  
+        overlay.classList.remove('fade-out');   
         overlay.classList.add('fade-in');
     }
 
